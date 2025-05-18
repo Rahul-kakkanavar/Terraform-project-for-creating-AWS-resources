533747674759daea62effad23e3f0240174f286f
Rahul kakkanavar <93816260+Rahul-kakkanavar@users.noreply.github.com>
2025-05-18 15:37:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region     = "ap-south-1"
-    access_key = "AKIA6JKEYFIVNUPHT5OR"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
 
 resource "aws_instance" "web" {
